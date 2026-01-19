65893d7f7face0b93e3153313fdb4c315d2f9d62
1194837305 <62242818+1194837305@users.noreply.github.com>
2026-01-19 21:26:39 +0800

    Update update_daily_summary.php

--- update_daily_summary.php
+++ update_daily_summary.php
@@ -4,7 +4,7 @@
 // 数据库连接配置
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'zqz_voting');
-define('DB_PASSWORD', '789789'); // 替换为你的密码
+define('DB_PASSWORD', ''); // 替换为你的密码
 define('DB_NAME', 'zqz_voting');
 
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
